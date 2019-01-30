2b857db5b4b49bbf5a9b12401b6a5f3cc2732052
generalpask <32198850+generalpask@users.noreply.github.com>
2019-01-30 20:55:04 +0100

    almost the last commit

--- push.bat
+++ push.bat
@@ -1,11 +1,15 @@
 @echo off
 
+echo.
 set /p commit="Name the commit: "
 echo.
 git add .
+echo.
+echo [92mGIT:[0m Added changes to index
+echo.
 git commit -m "%commit%"
 echo.
-echo [92mGIT:[0m Committed to repo [93mENG6_literary_periods_website[0m with commit message "%commit%"
+echo [92mGIT:[0m Committed to repo [93mENG6_literary_periods_website[0m with message "%commit%"
 echo.
 git push origin master
 echo.
