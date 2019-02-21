0df6f512b3875552135ad13a05b9607905aa621f
generalpask <32198850+generalpask@users.noreply.github.com>
2019-02-21 12:08:53 +0100

    daycycle progress

--- public/scripts/brightness.js
+++ public/scripts/brightness.js
@@ -1,22 +1,24 @@
 (function() {
 
-    var current_hour = new Date().getHours();
-
-
+    //var current_hour = new Date().getHours();
+    var current_hour = 18;
+    
     if (0 <= current_hour&&current_hour < 5) { // Night
         $(".body-1").css("background-image", 'url("public/img/souls_of_acheron.jpg")');
         $(".header, .link-div").css("background-image", 'radial-gradient(#5e5e5e, #454545)');
     }
     
     if (5 <= current_hour&&current_hour < 11) { // Morning
-        //document.write("<link rel='stylesheet' href='morning.css' type='text/css'>");
+        $(".body-1").css("background-image", 'url("public/img/Dali-Dream-of-Narcissus-TateModern-crop.png")')
+        $(".header, .link-div").css("background-image", 'radial-gradient(#f58628, #bb5700)');
     }
     if (11 <= current_hour&&current_hour < 16) { // Day
-        $(".body-1").css("backhground-image", 'url("public/img/adolf-hiremy-hirschl-ahasuerus-at-the-end-of-the-world-1888-trivium-art-history.jpg")');
-        $(".header, .link-div ").css("background-image", 'radial-gradient(#efefef, #8c8c8c)');
+        $(".body-1").css("background-image", 'url("public/img/adolf-hiremy-hirschl-ahasuerus-at-the-end-of-the-world-1888-trivium-art-history.jpg")');
+        $(".header, .link-divs").css("background-image", 'radial-gradient(#efefef, #8c8c8c)');
     }
     if (16 <= current_hour&&current_hour < 22) { // Evening
-        //document.write("<link rel='stylesheet' href='evening.css' type='text/css'>");
+        $(".header, .link-div").css("background-image", "radial-gradient(#100ea9, #01004c)");
+        
     }
     if (22 <= current_hour&&current_hour < 24) { // Night
         $(".body-1").css("background-image", 'url("public/img/souls_of_acheron.jpg"');
