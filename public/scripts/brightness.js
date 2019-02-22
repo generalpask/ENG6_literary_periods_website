7d99c7b03e6e699785318cde554bc94760c5e371
generalpask <32198850+generalpask@users.noreply.github.com>
2019-02-22 20:04:05 +0100

    various formatting

--- public/scripts/brightness.js
+++ public/scripts/brightness.js
@@ -6,6 +6,7 @@
     if (0 <= current_hour&&current_hour < 5) { // Night
         $(".body-1").css("background-image", 'url("public/img/souls_of_acheron.jpg")');
         $(".header, .link-div").css("background-image", 'radial-gradient(#5e5e5e, #454545)');
+        $("p, h1, a").css("color", "#e7dac7");
     }
     
     if (5 <= current_hour&&current_hour < 11) { // Morning
@@ -18,11 +19,13 @@
     }
     if (16 <= current_hour&&current_hour < 22) { // Evening
         $(".header, .link-div").css("background-image", "radial-gradient(#100ea9, #01004c)");
+        $("p, h1, a").css("color", "#e7dac7");
         
     }
     if (22 <= current_hour&&current_hour < 24) { // Night
         $(".body-1").css("background-image", 'url("public/img/souls_of_acheron.jpg"');
         $(".header, .link-div").css("background-image", 'radial-gradient(#5e5e5e, #454545)');
+        $("p, h1, a").css("color", "#e7dac7");
     }
 
 }).call(this);
