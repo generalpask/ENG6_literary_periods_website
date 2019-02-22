c3dbd6d7a4cf60c9e7229559ac50ee312a86e57b
generalpask <32198850+generalpask@users.noreply.github.com>
2019-02-22 23:17:27 +0100

    changes

--- public/scripts/brightness.js
+++ public/scripts/brightness.js
@@ -1,12 +1,13 @@
 (function() {
 
     //var current_hour = new Date().getHours();
-    var current_hour = 12;
+    var current_hour = 4;
     
     if (0 <= current_hour&&current_hour < 5) { // Night
         $(".body-1").css("background-image", 'url("public/img/souls_of_acheron.jpg")');
         $(".header, .link-div").css("background-image", 'radial-gradient(#5e5e5e, #454545)');
         $("p, h1, a").css("color", "#e7dac7");
+        $(".article-content").css("background-image", 'radial-gradient(#5e5e5e, #454545)');
     }
     
     if (5 <= current_hour&&current_hour < 11) { // Morning
@@ -16,6 +17,7 @@
     if (11 <= current_hour&&current_hour < 16) { // Day
         $(".body-1").css("background-image", 'url("public/img/adolf-hiremy-hirschl-ahasuerus-at-the-end-of-the-world-1888-trivium-art-history.jpg")');
         $(".header, .link-divs").css("background-image", 'radial-gradient(#efefef, #8c8c8c)');
+        $(".article-content").css("background-image", 'radial-gradient(#ffffff, #a7a7a7)');
     }
     if (16 <= current_hour&&current_hour < 22) { // Evening
         $(".header, .link-div").css("background-image", "radial-gradient(#100ea9, #01004c)");
@@ -23,9 +25,10 @@
         
     }
     if (22 <= current_hour&&current_hour < 24) { // Night
-        $(".body-1").css("background-image", 'url("public/img/souls_of_acheron.jpg"');
+        $(".body-1").css("background-image", 'url("public/img/souls_of_acheron.jpg")');
         $(".header, .link-div").css("background-image", 'radial-gradient(#5e5e5e, #454545)');
         $("p, h1, a").css("color", "#e7dac7");
+        $(".article-content").css("background-image", 'radial-gradient(#5e5e5e, #454545)');
     }
 
 }).call(this);
